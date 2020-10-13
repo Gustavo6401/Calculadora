73c38ae470540bf45995bf1aee0eceaf8637032e
Gustavo Oliveira <gustavometalmario@hotmail.com>
2020-10-12 21:59:33 -0300

    Esta é minha calculadora.

---
+++ ConsoleApp1/ConsoleApp1/Calculadora.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Calculadora
+    {
+        public int Somar(int número1, int número2)
+        {
+            int Soma = número1 + número2;
+            return Soma;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Me Informe O Primeiro número: ");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Me Informe O Segundo Número");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+
+            Calculadora C = new Calculadora();
+            int Soma = C.Somar(n1, n2);
+
+            Console.WriteLine("A Soma é: " + Soma + "!");
+
+            Console.ReadKey();
+        }
+    }
+}
